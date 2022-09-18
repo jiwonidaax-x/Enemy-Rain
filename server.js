0bc152112ff346ed6245fd850c9383d1a408ea17
jiwonidaax-x <jgb07012@gmail.com>
2022-09-18 16:06:54 -0400

    server

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var app= express();
+var HTTP_PORT = process.env.PORT || 8080;
+var path = require("path");
+
+app.get('/',(req,res)=>{
+res.sendFile(path.join(__dirname,"/views/index.html"));
+});
+
+app.get('/game',(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/game.html"));
+});
+
+app.get('/description',(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/description.html"));
+});
+
+app.use((req,res)=>{
+    res.status(404).send("Can't road Page!");
+})
+
+onHttpServer=()=>{
+    console.log(`server is running on ${HTTP_PORT}`);
+}
+
+app.use(express.static('public'));
+
+app.listen(HTTP_PORT,onHttpServer());
