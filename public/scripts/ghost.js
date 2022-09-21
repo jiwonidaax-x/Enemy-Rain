1256c0bc555be20519c0655d45c1255b12c839ec
jiwonidaax-x <jgb07012@gmail.com>
2022-09-21 18:07:51 -0400

    ghost

--- public/scripts/ghost.js
+++ public/scripts/ghost.js
@@ -1,17 +1,34 @@
+randomRange = (min, max) => {
+  return Math.floor(Math.random() * (max + 1 - min)) + min;
+};
+
 addghost = () => {
   const ghostdiv = document.createElement("div");
-  const bgelement = document.getElementById('bg');
-  const randomnum = Math.floor(Math.random() * 755);
-  ghostdiv.style.position = 'absolute';
-  ghostdiv.style.top = '0';
-  ghostdiv.style.left = randomnum + 'px';
+  const bgelement = document.getElementById("bg");
+  let randomnum = randomRange(0, BG_WIDTH - GHOST_WIDTH);
+  ghostdiv.style.position = "absolute";
+  ghostdiv.style.top = "0";
+  ghostdiv.style.left = randomnum + "px";
 
-  ghostdiv.style.width = GHOST_WIDTH + 'px';
-  ghostdiv.style.height = GHOST_HEIGHT + 'px';
+  ghostdiv.style.width = GHOST_WIDTH + "px";
+  ghostdiv.style.height = GHOST_HEIGHT + "px";
   ghostdiv.style.background = 'url("./images/ghost.png") no-repeat';
 
   bgelement.appendChild(ghostdiv);
-}
 
+  let interval = setInterval(() => {
+    let ghostTop = ghostdiv.style.top;
+    let topnum = strToNum(ghostTop);
+    ghostdiv.style.top = topnum + randomRange(1, 10) + "px";
+
+    if (topnum > BG_HEIGHT - GHOST_HEIGHT) {
+      ghostdiv.remove();
+      clearInterval(interval);
+    }
+  }, 100);
+};
+
+addghost();
+addghost();
 addghost();
 addghost();
--- public/scripts/hero.js
+++ public/scripts/hero.js
@@ -1,25 +1,29 @@
 const heroElement = document.getElementById('hero');
 
-document.addEventListener('keydown', function(e) {
+strToNum=(str)=>{
   var num = "";
-  const left = getComputedStyle(heroElement).left;
-  console.log(left);
-  for (let i = 0; i < left.length; i++) {
-    if (isNaN(Number(left[i])) == false) {
-      num += String(left[i]);
+  for (let i = 0; i < str.length; i++) {
+    if (isNaN(Number(str[i])) == false) {
+      num += String(str[i]);
     }
   }
   var leftval = Number(num);
+  return leftval;
+}
+document.addEventListener('keydown', function(e) {
+  const left = getComputedStyle(heroElement).left;
+  var leftval = strToNum(left);
   if (e.keyCode === 37 && leftval > 0) {
     heroElement.className = 'left';
-    heroElement.style.left = leftval - 1 + 'px';
+    heroElement.style.left = leftval - 5 + 'px';
   }
   else if (e.keyCode === 39 && leftval < 765) {
     heroElement.className = 'right';
-    heroElement.style.left = leftval + 1 + 'px';
+    heroElement.style.left = leftval + 5 + 'px';
   }
 });
 
 document.addEventListener('keyup', function() {
   heroElement.className = 'stop';
 });
+
