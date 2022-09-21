95149c5a4e1eaab038abfdc6428c4650a009ee24
jiwonidaax-x <jgb07012@gmail.com>
2022-09-21 14:04:52 -0400

    movement

--- public/scripts/hero.js
+++ public/scripts/hero.js
@@ -1,8 +1,30 @@
 const heroElement = document.getElementById('hero');
 
 document.addEventListener('keydown', function(e) {
+  var num = "";
   if (e.keyCode === 37) {
+    const left = getComputedStyle(heroElement).left;
+    console.log(left);
+    for (let i = 0; i < left.length; i++) {
+      if (isNaN(Number(left[i])) == false) {
+        num += String(left[i]);
+      }
+    }
+    var leftval = Number(num);
+    heroElement.style.left = leftval - 1 + 'px';
   }
-  else if (e.keyCode == 39) {
+  else if (e.keyCode === 39) {
+    const left = getComputedStyle(heroElement).left;
+    console.log(left);
+    for (let i = 0; i < left.length; i++) {
+      if (isNaN(Number(left[i])) == false) {
+        num += String(left[i]);
+      }
+    }
+    var leftval = Number(num);
+    heroElement.style.left = leftval + 1 + 'px';
+
   }
+
+
 });
