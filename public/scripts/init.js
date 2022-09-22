d049a4a17abefe265a4241f296807695780ba233
jiwonidaax-x <jgb07012@gmail.com>
2022-09-22 18:16:52 -0400

    time

--- public/scripts/init.js
+++ public/scripts/init.js
@@ -6,13 +6,19 @@ const scoreElement=document.getElementsByClassName('score')[0];
 function start() {
   console.log("btn");
   addghost();
-  let ghostrain=setInterval(addghost, 3000);
+  let ghostrain=setInterval(addghost, 2500);
   var timer=setInterval(function(){
   let str=timeElement.textContent;
   var time=str.split('s');
   console.log(time);
   time=time[0]-1;
   timeElement.textContent=time+"s";
+
+  if(time==10){
+    console.log("color");
+    timeElement.style.color=" #e94b48";
+    timeElement.classList.add("vibration");
+  }
   if(time==4){
     clearInterval(ghostrain)
   }
